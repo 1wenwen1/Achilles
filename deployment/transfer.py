09a60cd4fab04ec178ba88a5f6c60d0a03ea9bf7
1wenwen1 <wen19970221@gmail.com>
2025-02-20 06:18:38 +0800

    add docs

--- deployment/transfer.py
+++ deployment/transfer.py
@@ -10,17 +10,14 @@ ips = [ip.strip() for ip in ips]
 
 # Define the SCP command
 def scp_command(ip):
-    source_file = '/root/archive.tar.gz'
-    source_file1 = '/root/init.sh'
-    source_file2 = '/root/init2.sh'
+    source_file = '/root/damysus_updated/deployment/archive.tar.gz'
+    source_file1 = '/root/damysus_updated/deployment/init.sh'
     destination = f'root@{ip}:/root/'
     command = f'scp -i /root/damysus_updated/TShard  -o StrictHostKeyChecking=no {source_file} {destination}'
     command1 = f'scp -i /root/damysus_updated/TShard {source_file1} {destination}'
-    command2 = f'scp -i /root/damysus_updated/TShard {source_file2} {destination}'
     try:
         subprocess.run(command, shell=True, check=True)
         subprocess.run(command1, shell=True, check=True)
-        subprocess.run(command2, shell=True, check=True)
         print(f"Successfully transferred file to {ip}")
     except subprocess.CalledProcessError as e:
         print(f"Failed to transfer file to {ip}: {e}")
